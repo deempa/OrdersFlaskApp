069a691eda5a5c31ead3658edd222a1104b639ee
Lior Rabanian <masrab11@gmail.com>
2023-06-06 00:06:24 +0300

    1.0

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,8 +1,8 @@
 import requests
 
-IP = requests.get('https://api.ipify.org').content.decode('utf8')
-
 def test_add_new_order():
+    IP = requests.get('https://api.ipify.org').content.decode('utf8')
+    print(IP)
     url = f'http://{IP}:8087/add_new_order'
     order = {'name': 'ליאור', 'phone': '0505567800', 'address': 'מגדל שורשן 2', 'shipment_date': '2023-06-01T01:35', 'paid': 'שולם',\
         'delivered': 'נמסר', 'payment_method': 'מזומן', 'quantity': 2}
