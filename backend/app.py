6031ecbb6bd2ddc1fcad3e8fc3cecc1e076aa6cc
Lior Rabanian <masrab11@gmail.com>
2023-06-08 15:12:36 +0300

    3.0

--- backend/app.py
+++ backend/app.py
@@ -25,10 +25,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://" + os.getenv("DATABASE
 
 db.init_app(app)
 
-# if __name__ != '__main__':
-#     gunicorn_logger = logging.getLogger('gunicorn.error')
-#     app.logger.handlers = gunicorn_logger.handlers
-#     app.logger.setLevel(gunicorn_logger.level)
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 #     app.logger.setLevel(logging.DEBUG)
 
 app.logger.info("Starting Orders Management App")
